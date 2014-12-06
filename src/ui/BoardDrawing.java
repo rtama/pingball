9f3ad07e9d4d5ad728d423c66f71f8ad125c6ad2
Reece Tamashiro <rtama@mit.edu>
2014-12-06 15:26:12 -0500

    added the boarddrawing class to separate the drawing from the overall GUI

---
+++ src/ui/BoardDrawing.java
@@ -0,0 +1,11 @@
+package ui;
+
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class BoardDrawing extends JPanel implements KeyListener{
+
+    // TODO: I'll letchu move the stuff, I don't wanna break anything.
+    
+}
